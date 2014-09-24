aa31e5ebbf995c67587640828c63b112dffce9cb
Emba Moussa <emoussa@intellicentrics.com>
2014-09-24 16:54:54 +0100

    problem 5 solved

---
+++ problem5.rb
@@ -0,0 +1,31 @@
+require 'active_support/core_ext/enumerable'
+require 'benchmark'
+
+def factorize(n)
+ f = 2
+ factors = []
+ until n == 1 do
+  if n%f == 0
+   n = n / f
+   factors << f
+  else
+   f += 1
+  end
+ end
+ factors
+end
+
+n_factors = Hash.new(0)
+
+1.upto(10) do |a|
+ factorize(a).group_by { |v| v }.each do |max_factor|
+ 	n_factors[max_factor[0]] = max_factor[1] if max_factor[1].size > n_factors[max_factor[0]].size || !n_factors.has_key?(max_factor[0])
+ end
+end
+
+puts "-------"
+puts n_factors.inspect
+puts "-------"
+smallect_common_divisible = 1
+n_factors.each {|k,v| smallect_common_divisible *= v.inject(:*) }
+puts smallect_common_divisible

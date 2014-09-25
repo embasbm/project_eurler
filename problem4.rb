eeff380a15b76b7f94be4781ef00764de4ba7d8c
Emba Moussa <emoussa@intellicentrics.com>
2014-09-25 12:09:20 +0100

    problem 4 refactored and working correctly

--- problem4.rb
+++ problem4.rb
@@ -4,10 +4,6 @@ require 'benchmark'
 def palindrome_check(n)
  str = n.to_s
  for i in 0...str.length/2
-  print "str[" + i.to_s + "] " + str[i].to_s
-  print " == "
-  print "str[" + (str.length-i-1).to_s + "] " + str[str.length-i-1].to_s
-  print "\n"
   if str[i] != str[str.length-i-1]
    return false
   end
@@ -15,20 +11,20 @@ def palindrome_check(n)
  true
 end
 
-# puts palindrome_check(90449)
-# puts palindrome_check(90309)
-# puts palindrome_check(9449)
-# puts palindrome_check(9)
-# puts palindrome_check(949)
 
 def biggest_palindrome(n_digits)
  num = ("9"*n_digits).to_i
- num.downto(0) do |aa|
-  num.downto(0) do |bb|
-   p = aa * bb
-   return p if palindrome_check(p)
+ pals = []
+ (1).upto(num) do |num_1|
+  (1).upto(num) do |num_2|
+    p = num_1 * num_2
+    pals << p if palindrome_check(p)
   end
  end
+  pals.max
 end
 
-puts biggest_palindrome(2)
+
+print "\n\nThe largest palindrome:\t" + biggest_palindrome(3).to_s + "\n\n"
+
+

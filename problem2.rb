2277b004e84d45d3af4f3eb070cc2d69d5d3052e
Emba Moussa <emoussa@intellicentrics.com>
2014-09-25 10:17:10 +0100

    problem 2 refactored

--- problem2.rb
+++ problem2.rb
@@ -2,15 +2,17 @@ require 'active_support/core_ext/enumerable'
 require 'benchmark'
 
 
-n1 = 1
-n2 = 1
-sum = 0
+num_1, num_2, sum = 1,1,0
 
-while n1 < 4_000_000
- sum +=n1 if n1 % 2 == 0
- t= n1
- n1=n2
- n2= t + n2
+while num_1 < 4_000_000
+ sum += num_1 if num_1 % 2 == 0 
+ temp = num_1
+ num_1 = num_2
+ num_2 = temp + num_2
 end
 
-print "\n\n\n" +sum.to_s
+puts "\n\n"
+
+print "The sum of the even-valued terms:\t" + sum.to_s
+
+puts "\n\n"
